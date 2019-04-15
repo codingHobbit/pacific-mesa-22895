ccf5181688e1a203c77486a5bc32bc10468686ca
codingHobbit <49631773+codingHobbit@users.noreply.github.com>
2019-04-15 17:19:21 +0530

    First Changes for new list view

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,80 +1,108 @@
-import React, { Component } from 'react';
-import axios, { post } from 'axios';
-import ReactSvgPieChart from "react-svg-piechart"
-import Button from '@material-ui/core/Button';
-import Input from '@material-ui/core/Input';
-import './home.css';
+import React, { Component } from "react";
+import axios, { post } from "axios";
+import ReactSvgPieChart from "react-svg-piechart";
+import Button from "@material-ui/core/Button";
+import Input from "@material-ui/core/Input";
+import "./home.css";
 
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state ={
-      file:null,
+    this.state = {
+      file: null,
       data: []
-    }
-    this.onFormSubmit = this.onFormSubmit.bind(this)
-    this.onChange = this.onChange.bind(this)
-    this.fileUpload = this.fileUpload.bind(this)
+    };
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.fileUpload = this.fileUpload.bind(this);
     this.onClickHandler = this.onClickHandler.bind(this);
   }
-  onFormSubmit(e){
-    e.preventDefault() // Stop form submit
-    this.fileUpload(this.state.file).then((response)=>{
+  onFormSubmit(e) {
+    e.preventDefault(); // Stop form submit
+    this.fileUpload(this.state.file).then(response => {
       console.log(response.data);
-      this.setState({data:[
-        {title: "Unfilled", value: (response.data.total_volume - response.data.filled_volume), color: "#282c34"},
-        {title: "Filled", value: response.data.filled_volume, color: "#4c8cbd"}
-      ]})
-    })
+      this.setState({
+        data: [
+          {
+            title: "Unfilled",
+            value: response.data.total_volume - response.data.filled_volume,
+            color: "#282c34"
+          },
+          {
+            title: "Filled",
+            value: response.data.filled_volume,
+            color: "#4c8cbd"
+          }
+        ]
+      });
+    });
   }
   onChange(e) {
-    this.setState({file:e.target.files[0]})
+    this.setState({ file: e.target.files[0] });
   }
-  fileUpload(file){
-    const url = 'https://secret-river-30602.herokuapp.com/get_volume_ratio';
+  fileUpload(file) {
+    const url = "https://secret-river-30602.herokuapp.com/get_volume_ratio";
     const formData = new FormData();
-    formData.append('document',file)
+    formData.append("document", file);
     const config = {
-        headers: {
-            'content-type': 'multipart/form-data'
-        }
-    }
-    return  post(url, formData,config)
+      headers: {
+        "content-type": "multipart/form-data"
+      }
+    };
+    return post(url, formData, config);
   }
 
-  onClickHandler(){
-    this.setState({data: []});
+  onClickHandler() {
+    this.setState({ data: [] });
   }
 
   render() {
-    var paddingData = this.state.data.length>0 ? '0px' : '0px';
+    var paddingData = this.state.data.length > 0 ? "0px" : "0px";
     return (
-      <div style={{padding:paddingData}}>
-      <form style={{padding:"0px"}} onSubmit={this.onFormSubmit}>
-        <h1>Upload the numpy file representing the space platform</h1>
-        <br />
-        <br />
-        <input type="file" name="name" onChange={this.onChange} className="InputFile"/>
-        <Button variant="contained" color="primary" type="submit">Load chart</Button>
-      </form>
-      <div style={{height: '350px', width: '350px', display: 'inline-block', padding: '100px'}}>
-      <ReactSvgPieChart
-        data={this.state.data}
-        // If you need expand on hover (or touch) effect
-        expandOnHover={true}
-        expandSize={1}
-        shrinkOnTouchEnd={false}
-        strokeColor="#fff"
-        strokeLinejoin="round"
-        strokeWidth={1}
-        viewBoxSize={100}
-      />
+      <div style={{ padding: paddingData }}>
+        <form style={{ padding: "0px" }} onSubmit={this.onFormSubmit}>
+          <h1>Upload the numpy file representing the space platform</h1>
+          <br />
+          <br />
+          <input
+            type="file"
+            name="name"
+            onChange={this.onChange}
+            className="InputFile"
+          />
+          <Button variant="contained" color="primary" type="submit">
+            Load chart
+          </Button>
+          <Button
+            style={{ marginLeft: "20px" }}
+            variant="contained"
+            onClick={this.onClickHandler}
+          >
+            Reset
+          </Button>
+        </form>
+        <div
+          style={{
+            height: "250px",
+            width: "250px",
+            display: "inline-block",
+            padding: "50px"
+          }}
+        >
+          <ReactSvgPieChart
+            data={this.state.data}
+            // If you need expand on hover (or touch) effect
+            expandOnHover={true}
+            expandSize={1}
+            shrinkOnTouchEnd={false}
+            strokeColor="#fff"
+            strokeLinejoin="round"
+            strokeWidth={1}
+            viewBoxSize={100}
+          />
+        </div>
       </div>
-      <div>
-        <Button variant="contained" onClick={this.onClickHandler}>Reset</Button>
-      </div>
-      </div>
-   )
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import Home from './components/Home.js';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route, Link, BrowserRouter as Router, Switch } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Home from "./components/Home.js";
+import * as serviceWorker from "./serviceWorker";
 
 const routing = (
   <Router>
     <div>
-      <Route exact path="/" component={App} />
-      <Route path="/home" component={Home} />
+      <header className="App-header">
+        <p style={{ marginLeft: "20px", fontFamily: "sans-serif" }}>
+          Trap Water
+        </p>
+        {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-link">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="nav-link">
+              <Link to="/list">History</Link>
+            </li>
+          </ul>
+        </nav> */}
+      </header>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/list" component={History} />
+      </Switch>
     </div>
   </Router>
-)
+);
 
-ReactDOM.render(routing, document.getElementById('root'));
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
